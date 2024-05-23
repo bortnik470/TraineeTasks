9f0141a96bc950842e4a0999b1005b49d1794d88
Anatolii Bortnyk <anatolii.bortnyk@nice.com>
2024-05-23 14:19:07 +0300

    delete TestWebApi

--- StudentApi/Program.cs
+++ StudentApi/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
                 .AddNewtonsoftJson();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(new StudentRepoLogic(new DataAccessConnected()));
